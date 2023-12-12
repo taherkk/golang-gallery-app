f509a70cc298667bd7f22ea25638add418dd3a96
taherk <tkkathana@gmail.com>
2023-12-12 20:11:00 +0530

    Add create user controller and sign up functionality

---
+++ controllers/users.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Users struct {
+	Templates struct {
+		New Template
+	}
+}
+
+func (u Users) New(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.New.Execute(w, data)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))
+	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+}
--- main.go
+++ main.go
@@ -16,7 +16,13 @@ func main() {
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "sign-up.gohtml", "tailwind.gohtml"))))
+
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "sign-up.gohtml", "tailwind.gohtml"))
+
+	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
