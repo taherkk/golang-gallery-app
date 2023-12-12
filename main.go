a65e4a688e9d772a2c6e4310fcabd42f0c9dce24
taherk <tkkathana@gmail.com>
2023-12-12 19:22:00 +0530

    Creating sign-up page

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "sign-up.gohtml", "tailwind.gohtml"))))
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
